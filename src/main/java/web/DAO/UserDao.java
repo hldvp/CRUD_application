50b26b1fa41bf398251d4d9b654f224942f7efb2
hldvp <hldvp@yandex.ru>
2021-09-27 20:47:44 +0300

    Настройка DAO и Service слоя. Реализация необходимых методов.

--- src/main/java/web/DAO/UserDao.java
+++ src/main/java/web/DAO/UserDao.java
@@ -1,4 +1,13 @@
 package web.DAO;
 
+import web.model.User;
+
+import java.util.List;
+
 public interface UserDao {
+    public List<User> getAllUsers();
+    public User getUserById(long id);
+    public void creatUser(User user);
+    public void updateUser(User user);
+    public void deleteUser(long id);
 }
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,4 +1,40 @@
 package web.service;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.DAO.UserDao;
+import web.model.User;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserDao userDao;
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public User getUserById(long id) {
+        return userDao.getUserById(id);
+    }
+
+    @Override
+    public void creatUser(User user) {
+        userDao.creatUser(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userDao.updateUser(user);
+    }
+
+    @Override
+    public void deleteUser(long id) {
+        userDao.deleteUser(id);
+    }
 }
